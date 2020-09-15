7f5cb57f1b2c0744bf64b0bd2bfe9895dda5e522
Arthur T <act5924@g.rit.edu>
2020-09-15 18:23:08 -0400

    Plot class average works

--- plots.py
+++ plots.py
@@ -59,6 +59,19 @@ def get_average(filename, grade_item):
             count += 1
         return total_grade/count
 
+def get_average_new(filename, col):
+    with open(filename) as file:
+        header_fields = next(file).split(',')
+        total_grade = 0
+        count = 0
+        for line in file:
+            fields = line.split(',')
+            if fields[col] == '':
+                fields[col] = 0
+            total_grade += float(fields[col])
+            count += 1
+        return total_grade/count
+
 def plot_grades(filename, first, last):
     with open(filename) as file:
         header_fields = next(file).split(',')
@@ -75,3 +88,17 @@ def plot_grades(filename, first, last):
                 return True
         
         return False
+    
+def plot_class_averages(filename):
+    with open(filename) as file:
+        header_fields = next(file).split(',')
+        plotter.init("Class Average(" + str(filename) + ')', "Labs", "Score")
+        col = 2
+        for i in range(16):
+            plotter.add_data_point(float(get_average_new(filename, col)))
+            col += 1
+        plotter.plot()
+        var = input('Enter to quit: ')
+
+plot_class_averages('data/grades_010.csv')
+plot_class_averages('data/grades_363.csv')
