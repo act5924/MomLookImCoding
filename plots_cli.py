80a6eec61bf5da48863e133405d89c7dc684c9ee
Arthur T <act5924@g.rit.edu>
2020-09-17 21:16:39 -0400

    Part 3

--- plots_cli.py
+++ plots_cli.py
@@ -11,4 +11,24 @@ def quit():
         return True
     else:
         return False
+    
+def main():
+    print ('>> ')
+    print ('Enter a command or \'quit\' to quit')
+    command = input('>> ')
+    command_split = command.split()
+    while True:
+        try:
+            if command_split[0] == 'quit':
+                if quit() == True:
+                    print ('Goodbye!')
+                    break
+                else:
+                    raise IndexError
+        except IndexError:
+            print ('Enter a command or \'quit\' to quit')
+            command = input('>> ')
+            command_split = command.split()
 
+
+main()
