cdab6142e21f53932e648befa1fefb01a19e7673
Arthur T <act5924@g.rit.edu>
2020-09-17 21:09:07 -0400

    Part 2

---
+++ plots_cli.py
@@ -0,0 +1,14 @@
+'''
+Arthur Tonoyan
+Assignment 5.2
+'''
+
+
+
+def quit():
+    quit_quit = input('Are you sure (y/n) : ')
+    if quit_quit == 'y' or quit_quit == 'Y':
+        return True
+    else:
+        return False
+
