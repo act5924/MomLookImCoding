2aa53d9db992343549dfa1fcdb654a8363e5cc68
Arthur T <act5924@g.rit.edu>
2020-09-20 18:19:43 -0400

    Plots test Sion lab average test full grades format change

--- plots_test.py
+++ plots_test.py
@@ -7,7 +7,7 @@ import plots
 import testing
 
 def lab_average_test():
-    testing.assert_floats('lab_average', 80.66, plots.lab_average('data/grades_010.csv', 'Sion', 'Lobasso'), 0.05, True)
+    testing.assert_floats('lab_average', 80.66, plots.lab_average('data/full_grades_010.csv', 'Sion', 'Lobasso'), 0.05, True)
 
 def lab_average_test_None():
     assert(plots.lab_average('data/grades_010.csv', 'Bob', 'Bobson') == None)
@@ -30,13 +30,13 @@ def plot_grades_test_Jasmin():
     assert(plots.plot_grades('data/grades_363.csv', 'Jasmin', 'Skroch') == True)
 
 def main():
-    #lab_average_test()
+    lab_average_test()
     #lab_average_test_None()
     #get_average_test_Lab6()
     #get_average_test_Lab3()
     #get_average_test_None()
-    plot_grades_test_Sion()
-    plot_grades_test_Jasmin()
+    #plot_grades_test_Sion()
+    #plot_grades_test_Jasmin()
 
 if __name__ == '__main__':
     main()
